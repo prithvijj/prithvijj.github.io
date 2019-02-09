0f26be19c53180e3a23d36fef3c69bab5f72f34c
pbj <pbj@pop-os.localdomain>
2019-02-09 16:07:54 -0500

    Added Toggle Sidebar

---
+++ script.js
@@ -0,0 +1,32 @@
+// $(document).ready(function () {
+
+//     $("#sidebar").mCustomScrollbar({
+//          theme: "minimal"
+//     });
+
+//     $('#sidebarCollapse').on('click', function () {
+//         // open or close navbar
+//         $('#sidebar').toggleClass('active');
+//         // close dropdowns
+//         $('.collapse.in').toggleClass('in');
+//         // and also adjust aria-expanded attributes we use for the open/closed arrows
+//         // in our CSS
+//         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+//     });
+
+// });
+
+
+
+
+        $(document).ready(function () {
+            $("#sidebar").mCustomScrollbar({
+                theme: "minimal"
+            });
+
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar, #content').toggleClass('active');
+                $('.collapse.in').toggleClass('in');
+                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+            });
+        });
